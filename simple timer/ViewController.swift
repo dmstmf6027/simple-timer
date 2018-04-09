bb90241b7e78fdf04fa1d9693b5870fbb29a9233
201631053 <hubot@githhub@com>
2018-04-09 15:16:01 +0900

    add closure

--- simple timer/ViewController.swift
+++ simple timer/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timerSwitch: UISwitch!
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timerSwitch.setOn(false, animated: true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss a"
+        timeLabel.text = formatter.string(from: date)
     }
 
-
+    @IBAction func timerStart(_ sender: Any) {
+        if timerSwitch.isOn == true {
+            
+            //closure 사용
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block:{(myTimer) in self.updateTime()})
+        }   else {
+            myTimer.invalidate()
+        }
+    }
+    
 }
 
